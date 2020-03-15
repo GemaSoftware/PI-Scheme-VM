fb5186f9575efaccf525daf6d6d8091b1269747d
Agron G <agron.gemajli@uconn.edu>
2020-03-15 13:08:45 -0400

    moving to folders. sorting

--- runStartup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ip=$(ifconfig enp3s0 | grep 'inet ' | cut -d: -f2 | awk '{ print $2}')
-v=$(host "$ip" | awk '{ print $5}')
-echo "$ip"
-echo "$v"
-#hostnamectl set-hostname "$v"
--- windowsvm.bat
+++
@@ -1,2 +0,0 @@
-qemu-system-arm -M versatilepb -cpu arm1176 -m 256 -hda "./1.img" -net nic -net user,hostfwd=tcp::5022-:22 -dtb "./versatile-pb.dtb" -kernel "./qemukernel" -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" -no-reboot
-
